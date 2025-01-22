b615089d6db8339a95d2090255d7762806c45189
Phanindra <phanindragadireddy2@gmail.com>
2025-01-22 11:21:00 +0530

    options added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import Questions from './Questions.json'
 import Display from './Display';
 
 function App() {
+  const endQuiz = (marks) => {
+    alert(marks)
+  }
   return (
     <>
       {Questions.map((question, index) => {
         return (
-          <Display question = {question} index = {index} />
+         <div>
+         <Display question = {question} index = {index} endQuiz = {endQuiz}/>
+         </div>
         )
       })}
+     
     </>
   );
 }
--- src/Display.js
+++ src/Display.js
@@ -1,10 +1,20 @@
 import React from 'react'
 
-function Display({question, index}) {
+function Display({question, index, endQuiz}) {
+    function optionChange(e){
+        const temp = e.target.value === question.answer ? 1 : 0;
+        endQuiz(temp)
+    }
   return (
     <>
-        <div>
+        <div className='alignLeft'>
             <h3>{question.title}</h3>
+            <div onChange={optionChange}>
+                <input type='radio' value='A' name={index} />{question.options[0]} &nbsp;
+                <input type='radio' value='B' name={index} />{question.options[1]} &nbsp;
+                <input type='radio' value='C' name={index} />{question.options[2]} &nbsp;
+                <input type='radio' value='D' name={index} />{question.options[3]} &nbsp;
+            </div>
         </div>
     </>
   )
